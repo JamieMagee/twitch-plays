e53a92383329b94fbe2f1ec4e22b2ad97dcb7818
Jamie Magee <jamie.magee@gmail.com>
2014-02-18 16:29:21 +0000

    Change bytes() to encode() to ensure Python 2 back compatability

--- lib/irc.py
+++ lib/irc.py
@@ -29,9 +29,9 @@ class Irc:
 
         sock.settimeout(None)
 
-        sock.send(bytes('USER {}\r\n'.format(username), 'UTF-8'))
-        sock.send(bytes('PASS {}\r\n'.format(password), 'UTF-8'))
-        sock.send(bytes('NICK {}\r\n'.format(username), 'UTF-8'))
+        sock.send('USER {}\r\n'.format(username).encode('UTF-8'))
+        sock.send('PASS {}\r\n'.format(password).encode('UTF-8'))
+        sock.send('NICK {}\r\n'.format(username).encode('UTF-8'))
 
         if not self.check_login_status(self.recv()):
             pp('Invalid login.', 'error')
@@ -39,12 +39,12 @@ class Irc:
         else:
             pp('Login successful!')
 
-        sock.send(bytes('JOIN #{}\r\n'.format('twitchplayspokemon'), 'UTF-8'))
+        sock.send('JOIN #{}\r\n'.format('twitchplayspokemon').encode('UTF-8'))
         pp('Joined #%s\n\n' % 'twitchplayspokemon')
 
     def ping(self, data):
-        if data.startswith(bytes('PING', 'UTF-8')):
-            self.sock.send(data.replace(bytes('PING', 'UTF-8'), bytes('PONG', 'UTF-8')))
+        if data.startswith('PING'.encode('UTF-8')):
+            self.sock.send(data.replace('PING'.encode('UTF-8'), 'PONG'.encode('UTF-8')))
 
     def recv(self, amount=1024):
         return self.sock.recv(amount)
